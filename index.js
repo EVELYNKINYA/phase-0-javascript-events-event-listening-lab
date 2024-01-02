672b63e841e13a160b6e143ea573e5da4e636fdf
EVELYNKINYA <evelynkinya156@gmail.com>
2024-01-02 23:53:09 +0300

    DONE WITH ASSIGNMENT

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('input');
+  
+    // Add an event listener to the 'input' element
+    input.addEventListener('click', function() {
+      // Some event handling logic
+      console.log('Input clicked!');
+    });
+  }
+  
